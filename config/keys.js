3a67b5c857fbbf05fa309c52ff16eafc373f9634
Andy Dietler <andy@dietler.net>
2018-05-26 15:51:30 -0700

    Save profile done

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://andy:andy@ds249565.mlab.com:49565/devcondemo"
+  mongoURI: "mongodb://andy:andy@ds249565.mlab.com:49565/devcondemo",
+  secretOrKey: "secret"
 };
